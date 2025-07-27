d360d5f3eec70b40477dc53cbea5b40ee984d932
parker <pliabraaten@gmail.com>
2025-07-27 12:11:29 -0700

    Start menu option to query a specific time

--- userInterface.py
+++ userInterface.py
@@ -7,10 +7,10 @@ class CLI:
 
         print("This is the command-line interface")
         print("Enter 1 see total mileage of trucks")
-        print("Enter 2 to see all packages after delivery")
-        print("Enter 3 to see a single package after delivery")
-        print("Enter 4 to see the status of all packages after a certain time")
-        action = input("Enter your value: ")
+        print("Enter 2 to query all packages after delivery")
+        print("Enter 3 to lookup a package after delivery")
+        print("Enter 4 to query delivery status at a specific time")
+        action = input("Enter your option: ")
 
 
         # Enter 1 see total mileage of trucks
@@ -24,13 +24,17 @@ class CLI:
             print("Truck 3 mileage: " + str(round(truck3.mileage, 2)))
 
 
-        # Enter 2 to see all packages after delivery
+        # Enter 2 to query all packages after delivery
         if action == "2":
             package_hashmap.print_all()
 
 
-        # Enter 3 to see a single package after delivery
+        # Enter 3 to see a lookup a package after delivery
         if action == "3":
             id = int(input("Enter package id: "))
             package_hashmap.print_package(id)
 
+
+        # Enter 4 to query delivery status at a specific time
+        if action == "4":
+            return
