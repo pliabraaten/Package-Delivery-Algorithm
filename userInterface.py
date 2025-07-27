7fb88cdad2e19d6ed0600c6bfb99f90a4986d514
parker <pliabraaten@gmail.com>
2025-07-27 16:03:37 -0700

    Clean up interface format, return user back to menu after selection.

--- userInterface.py
+++ userInterface.py
@@ -7,11 +7,14 @@ class CLI:
 
     def menu(self):
 
+        print("\n----------------------------------------------------")
         print("This is the command-line interface")
+        print("----------------------------------------------------")
         print("Enter 1 see total mileage of trucks")
         print("Enter 2 to query all packages after delivery")
         print("Enter 3 to lookup a package after delivery")
         print("Enter 4 to query delivery status at a specific time")
+        print("--")
         action = input("Enter your option: ")
 
 
@@ -23,10 +26,15 @@ class CLI:
 
             total_mileage = truck1.mileage + truck2.mileage #+ truck3.mileage
 
+            print("\n----------------------------------------------------")
+            print("RESULT:")
             print("Total Miles: " + str(round(total_mileage, 2)))
             print("Truck 1 mileage: " + str(round(truck1.mileage, 2)))
             print("Truck 2 mileage: " + str(round(truck2.mileage, 2)))
             # print("Truck 3 mileage: " + str(round(truck3.mileage, 2)))
+
+            # Return to menu options
+            self.menu()
 
 
         # Enter 2 to query all packages after delivery
@@ -35,17 +43,28 @@ class CLI:
             stop_time = None
             fleetOperations.Fleet.run_package_delivery(stop_time)
 
+            print("\n----------------------------------------------------")
+            print("RESULT:")
             config.package_hashmap.print_all()
 
+            # Return to menu options
+            self.menu()
 
-        # Enter 3 to see a lookup a package after delivery
+
+        # Enter 3 to lookup a package after delivery
         if action == "3":
 
             stop_time = None
             fleetOperations.Fleet.run_package_delivery(stop_time)
 
             id = int(input("Enter package id: "))
+
+            print("\n----------------------------------------------------")
+            print("RESULT:")
             config.package_hashmap.print_package(id)
+
+            # Return to menu options
+            self.menu()
 
 
         # Enter 4 to query delivery status at a specific time
@@ -57,6 +76,17 @@ class CLI:
 
             # print(stop_time)
 
-            fleetOperations.Fleet.run_package_delivery(stop_time)
+            truck1, truck2 = fleetOperations.Fleet.run_package_delivery(stop_time)
+
+            print("Truck 1 delivered " + str(truck1.counter) + " packages")
+            print("Truck 2 delivered " + str(truck2.counter) + " packages")
+
+            # config.package_hashmap.print_all()
+
+            print("Delivered packages: ")
+            config.package_hashmap.print_delivered()
+
+            # Return to menu options
+            self.menu()
 
             return
